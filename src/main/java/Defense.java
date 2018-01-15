16b68384627581fa6f5ed9f4713b4c782a053c17
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-15 21:46:47 +0000

    Added healthValue and attackValue properties to Defense class with getters and tests

--- src/main/java/Defense.java
+++ src/main/java/Defense.java
@@ -1,12 +1,24 @@
 public class Defense {
 
     private String type;
+    private int healthValue;
+    private int attackValue;
 
-    public Defense(String type){
+    public Defense(String type, int healthValue, int attackValue){
         this.type = type;
+        this.healthValue = healthValue;
+        this.attackValue = attackValue;
     }
 
     public String getType() {
         return this.type;
+    }
+
+    public int getHealthValue() {
+        return this.healthValue;
+    }
+
+    public int getAttackValue() {
+        return this.attackValue;
     }
 }
--- src/test/java/DefenseTest.java
+++ src/test/java/DefenseTest.java
@@ -9,12 +9,22 @@ public class DefenseTest {
 
     @Before
     public void before(){
-        defense = new Defense("Cannon");
+        defense = new Defense("Cannon", 1000, 50);
     }
 
     @Test
     public void hasType(){
         assertEquals("Cannon", defense.getType());
+    }
+
+    @Test
+    public void hasHealthValue(){
+        assertEquals(1000, defense.getHealthValue());
+    }
+
+    @Test
+    public void hasAttackValue(){
+        assertEquals(50, defense.getAttackValue());
     }
 
 
